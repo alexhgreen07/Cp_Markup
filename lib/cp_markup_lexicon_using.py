c35ff6fb94d22938193d43b7edb8909b9592eb68
Alex Green <alex.h.green07@gmail.com>
2012-12-29 17:55:50 -0500

    - Updated code comments for doxygen

--- lib/cp_markup_lexicon_using.py
+++ lib/cp_markup_lexicon_using.py
@@ -1,3 +1,7 @@
+## @package cp_markup_lexicon_using
+# @brief This file contains all lexical objects and functions for parsing
+# C+ using statements.
+
 import sys
 import os
 import time
@@ -14,6 +18,7 @@ from plex import *
 brace_counter = 0
 previous_state = ""
 
+## @brief This function is triggered when the starting keyword of the using statement is found.
 def Using_Definition_Start(scanner,text):
 	
 	global previous_state
@@ -21,18 +26,21 @@ def Using_Definition_Start(scanner,text):
 	
 	scanner.begin("using_scope_state_name")
 	
+## @brief This function is triggered when the name of the namespace being used is found.
 def Using_Definition_Name(scanner,text):
 	
 	print "Using namespace '%s'" % text
 	
 	scanner.begin("using_scope_state_terminator")
 
+## @brief This function is triggered when the using statement is complete.
 def Using_Definition_Complete(scanner,text):
 	
 	global previous_state
 	
 	scanner.begin(previous_state)
 
+## @brief This is a Plex state object for detecting the namespace name being used.
 using_scope_state_name = State(
 	"using_scope_state_name", [
 		(whitespace_chars,IGNORE),
@@ -42,6 +50,7 @@ using_scope_state_name = State(
 	]
 )
 
+## @brief This is a Plex state object for detecting the end of the using statement.
 using_scope_state_terminator = State(
 	"using_scope_state_terminator", [
 		(whitespace_chars,IGNORE),
@@ -51,15 +60,16 @@ using_scope_state_terminator = State(
 	]
 )
 
+## @brief This is the token for detecting the start of the using definition.
 token_tuple = (Str("using") + force_whitespace, Using_Definition_Start)
 
+## @brief This is a list of all states used for parsing the using definition.
 lexicon_list_states = []
 lexicon_list_states.append(using_scope_state_name)
 lexicon_list_states.append(using_scope_state_terminator)
 
-#master lexicon list
+## @brief This is the master lexicon list for parsing using definitions.
 lexicon_list = []
-
 lexicon_list = lexicon_list + lexicon_list_states
 
 def Get_Token_Tuples():
