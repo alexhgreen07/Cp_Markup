c82c8004d8ce7a23a0a56357ffbbb8f717b19b8c
Alex Green <alex.h.green07@gmail.com>
2012-12-29 21:40:49 -0500

    - Added a doxygen main page comments file.

---
+++ documentation/comments.py
@@ -0,0 +1,51 @@
+## @mainpage C+ Markup API Reference
+# This is the API reference documentation for the C+ markup translator. The 
+# software is written in Python using the Plex lexical parser.
+#
+# @section intro_sec Introduction
+# This document outlines all packages, classes, functions, and variables used for parsing 
+# C+ markup files. It contains all required information to properly use and update the code.
+# The main purpose of this document is to help in code updates and alterations.
+# 
+# @section outline_sec Outline
+# This is the API reference outline...
+# 
+# @section plex_sec Plex Library
+# Plex is a lexical analysis library used for parsing the C+ files...
+# 
+# 
+#
+# 
+# 
+#
+# 
+# 
+#
+# 
+# 
+#
+# 
+# 
+#
+# 
+# 
+#
+# 
+# 
+#
+# 
+# 
+#
+# 
+# 
+#
+# 
+# 
+#
+# 
+# 
+#
+# 
+# 
+#
+
